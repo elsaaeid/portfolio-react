300402512e38974523d24271e590a8d458276928
elsaaeid <saidsadaoy@gmail.com>
2022-09-23 07:13:07 +0200

    deploy my portfolio

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ class CTA extends Component {
     render() {
         return (
             <div className='cta'>
-            <a href="https://drive.google.com/file/d/1TjF2QMrJfGO-8EO3mZ4QN_rhQHhstqoU/view?usp=sharing" download className='btn'>Download CV</a>
+            <a href="https://drive.google.com/file/d/1UZmPMiY24uWySquaICjAjAQ_LyyEKt2X/view?usp=sharing" download className='btn'>Download CV</a>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 		    </div>
         )
