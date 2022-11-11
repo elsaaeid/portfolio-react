3a4fa1f8a61d1bcff2b6f18f74b6d11c724f485f
elsaaeid <saidsadaoy@gmail.com>
2022-11-11 08:27:32 +0200

    deploy update resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ class CTA extends Component {
     render() {
         return (
             <div className='cta'>
-            <a href="https://drive.google.com/file/d/1UZmPMiY24uWySquaICjAjAQ_LyyEKt2X/view?usp=sharing" download className='btn'>Download CV</a>
+            <a href="https://drive.google.com/file/d/1ttZ1QZW7AgSYyLXwT-urb1Ya5bvAQkH6/view?usp=share_link" download className='btn'>Download CV</a>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 		    </div>
         )
